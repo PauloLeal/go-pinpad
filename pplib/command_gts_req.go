a29427e51250cf00ef9d4af54b82e05c43d2d4ba
PauloLeal <paulolealdev@gmail.com>
2020-06-26 23:21:20 -0300

    gts req

---
+++ pplib/command_gts_req.go
@@ -0,0 +1,54 @@
+package pplib
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type GtsRequest struct {
+	AcquirerIndex string `json:"acquirer_index"`
+}
+
+func (cmd *GtsRequest) GetName() string {
+	return "GTS"
+}
+
+func (cmd *GtsRequest) Validate() error {
+	ac, err := strconv.Atoi(cmd.AcquirerIndex)
+	if err != nil {
+		return err
+	}
+
+	if ac < 0 || ac > 99 {
+		return errors.New("invalid AcquirerIndex value")
+	}
+	return nil
+}
+
+func (cmd *GtsRequest) Parse(rawData string) error {
+	pr := NewPositionalReader(rawData)
+
+	cmdName := pr.Read(3)
+	if cmdName != "GTS" {
+		return errors.New("cannot parse cmd command")
+	}
+
+	size, err := strconv.Atoi(pr.Read(3))
+	if err != nil {
+		return err
+	}
+
+	cmd.AcquirerIndex = pr.Read(size)
+
+	return cmd.Validate()
+}
+
+func (cmd *GtsRequest) String() string {
+	err := cmd.Validate()
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%s002%02s", cmd.GetName(), cmd.AcquirerIndex)
+}
---
+++ pplib/command_gts_req_test.go
@@ -0,0 +1,25 @@
+package pplib
+
+import (
+	"testing"
+)
+
+func TestGtsRequest_Validate(t *testing.T) {
+	t.Run("00", auxTestCommand_Validate(&GtsRequest{AcquirerIndex: "01"}, false))
+	t.Run("00", auxTestCommand_Validate(&GtsRequest{AcquirerIndex: "00"}, false))
+	t.Run("00", auxTestCommand_Validate(&GtsRequest{AcquirerIndex: "100"}, true))
+}
+
+func TestGtsRequest_Parse(t *testing.T) {
+	t.Run("00", auxTestCommand_Parse("GTS00200", &GtsRequest{}, &GtsRequest{AcquirerIndex: "00"}, false))
+	t.Run("01", auxTestCommand_Parse("GTS00201", &GtsRequest{}, &GtsRequest{AcquirerIndex: "01"}, false))
+	t.Run("no params", auxTestCommand_Parse("GTS000", &GtsRequest{}, &GtsRequest{AcquirerIndex: ""}, true))
+	t.Run("bad size", auxTestCommand_Parse("GTS003100", &GtsRequest{}, &GtsRequest{AcquirerIndex: "100"}, true))
+	t.Run("bad size format", auxTestCommand_Parse("GTS0AA00", &GtsRequest{}, &GtsRequest{AcquirerIndex: ""}, true))
+	t.Run("bad name", auxTestCommand_Parse("GTX00200", &GtsRequest{}, &GtsRequest{AcquirerIndex: ""}, true))
+}
+
+func TestGtsRequest_String(t *testing.T) {
+	t.Run("00", auxTestCommand_String(&GtsRequest{AcquirerIndex: "01"}, "GTS00201"))
+	t.Run("bad size", auxTestCommand_String(&GtsRequest{AcquirerIndex: "100"}, ""))
+}
