ecea6a90c090b2f621c7735c636e2466a8f9498d
PauloLeal <paulolealdev@gmail.com>
2020-06-27 20:22:20 -0300

    gts response

---
+++ pplib/command_gts_res.go
@@ -0,0 +1,55 @@
+package pplib
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type GtsResponse struct {
+	status    int
+	Timestamp string `json:"timestamp"`
+}
+
+func (cmd *GtsResponse) GetName() string {
+	return "GTS"
+}
+
+func (cmd *GtsResponse) Validate() error {
+	if len(cmd.Timestamp) != 10 {
+		return errors.New("invalid Timestamp value")
+	}
+	return nil
+}
+
+func (cmd *GtsResponse) Parse(rawData string) error {
+	pr := NewPositionalReader(rawData)
+
+	cmdName := pr.Read(3)
+	if cmdName != cmd.GetName() {
+		return errors.New(fmt.Sprintf("cannot parse %s command", cmd.GetName()))
+	}
+
+	status, err := strconv.Atoi(pr.Read(3))
+	if err != nil {
+		return err
+	}
+
+	timeStampSize, err := strconv.Atoi(pr.Read(3))
+	if err != nil {
+		return err
+	}
+
+	cmd.status = status
+	cmd.Timestamp = pr.Read(timeStampSize)
+
+	return cmd.Validate()
+}
+
+func (cmd *GtsResponse) String() string {
+	return fmt.Sprintf("%s%03d%03d%s", cmd.GetName(), cmd.status, len(cmd.Timestamp), cmd.Timestamp)
+}
+
+func (cmd *GtsResponse) GetStatus() int {
+	return cmd.status
+}
---
+++ pplib/command_gts_res_test.go
@@ -0,0 +1,25 @@
+package pplib
+
+import "testing"
+
+func TestGtsResponse_GetStatus(t *testing.T) {
+	t.Run("status 0", auxTestCommand_GetStatus(&GtsResponse{status: PP_OK}, PP_OK))
+}
+
+func TestGtsResponse_Validate(t *testing.T) {
+	t.Run("0", auxTestCommand_Validate(&GtsResponse{status: 0, Timestamp: "0000000000"}, false))
+	t.Run("empty", auxTestCommand_Validate(&GtsResponse{status: 0, Timestamp: ""}, true))
+	t.Run("<10", auxTestCommand_Validate(&GtsResponse{status: 0, Timestamp: "000000000"}, true))
+	t.Run(">10", auxTestCommand_Validate(&GtsResponse{status: 0, Timestamp: "00000000000"}, true))
+}
+
+func TestGtsResponse_Parse(t *testing.T) {
+	t.Run("status 0", auxTestCommand_Parse("GTS0000100000000000", &GtsResponse{}, &GtsResponse{status: PP_OK, Timestamp: "0000000000"}, false))
+	t.Run("bad status", auxTestCommand_Parse("GTS00A0100000000000", &GtsResponse{}, &GtsResponse{status: PP_OK, Timestamp: ""}, true))
+	t.Run("Bad command name", auxTestCommand_Parse("GTX000000", &GtsResponse{}, &GtsResponse{status: PP_OK}, true))
+	t.Run("bad command size", auxTestCommand_Parse("GTS00001A0000000000", &GtsResponse{}, &GtsResponse{status: PP_OK, Timestamp: ""}, true))
+}
+
+func TestGtsResponse_String(t *testing.T) {
+	t.Run("status 0", auxTestCommand_String(&GtsResponse{status: PP_OK}, "GTS000000"))
+}
