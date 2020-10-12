fdeb31a19d69d54cf900d1068f608b8dacdcc917
PauloLeal <paulolealdev@gmail.com>
2020-10-12 12:00:36 -0300

    clo

---
+++ bc/command_clo.go
@@ -0,0 +1,95 @@
+package bc
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type CloRequest struct {
+	IdleMessage string `json:"idle_message" pp_dataType:"A32"`
+}
+
+func (cmd *CloRequest) GetName() string {
+	return "CLO"
+}
+
+func (cmd *CloRequest) Validate() error {
+	return ValidateFieldsByDataTypeTag(*cmd)
+}
+
+func (cmd *CloRequest) Parse(rawData string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+
+	pr := NewPositionalReader(rawData)
+
+	cmdName := pr.Read(3)
+	if cmdName != cmd.GetName() {
+		return errors.New(fmt.Sprintf("cannot parse %s command", cmd.GetName()))
+	}
+
+	size, err := strconv.Atoi(pr.Read(3))
+	if err != nil {
+		return err
+	}
+
+	cmd.IdleMessage = pr.Read(size)
+
+	return cmd.Validate()
+}
+
+func (cmd *CloRequest) String() string {
+	err := cmd.Validate()
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%s032%32s", cmd.GetName(), cmd.IdleMessage)
+}
+
+type CloResponse struct {
+	status int
+}
+
+func (cmd *CloResponse) GetName() string {
+	return "CLO"
+}
+
+func (cmd *CloResponse) Validate() error {
+	return nil
+}
+
+func (cmd *CloResponse) Parse(rawData string) (err error) {
+	pr := NewPositionalReader(rawData)
+
+	cmdName := pr.Read(3)
+	if cmdName != cmd.GetName() {
+		return errors.New(fmt.Sprintf("cannot parse %s command", cmd.GetName()))
+	}
+
+	status, err := strconv.Atoi(pr.Read(3))
+	if err != nil {
+		return err
+	}
+
+	zeroes := pr.Read(3)
+	if zeroes != "000" {
+		return errors.New(fmt.Sprintf("cannot parse %s command", cmd.GetName()))
+	}
+
+	cmd.status = status
+
+	return cmd.Validate()
+}
+
+func (cmd *CloResponse) String() string {
+	return fmt.Sprintf("%s%03d000", cmd.GetName(), cmd.status)
+}
+
+func (cmd *CloResponse) GetStatus() int {
+	return cmd.status
+}
---
+++ bc/command_clo_test.go
@@ -0,0 +1,46 @@
+package bc
+
+import (
+	"testing"
+)
+
+const goodCloSample = "     go emv                     "
+
+func TestCloRequest_Validate(t *testing.T) {
+	t.Run("32", auxTestCommand_Validate(&CloRequest{IdleMessage: goodCloSample}, false))
+	t.Run("!32", auxTestCommand_Validate(&CloRequest{IdleMessage: "RETIRE O CARTAO"}, true))
+}
+
+func TestCloRequest_Parse(t *testing.T) {
+	t.Run("good", auxTestCommand_Parse("CLO032"+goodCloSample, &CloRequest{}, &CloRequest{IdleMessage: goodCloSample}, false))
+	t.Run("no params 15", auxTestCommand_Parse("CLO015RETIRE O CARTAO", &CloRequest{}, &CloRequest{}, true))
+	t.Run("no params 32", auxTestCommand_Parse("CLO032RETIRE O CARTAO", &CloRequest{}, &CloRequest{}, true))
+	t.Run("bad size", auxTestCommand_Parse("CLOA32"+goodCloSample, &CloRequest{}, &CloRequest{IdleMessage: goodCloSample}, true))
+	t.Run("bad name", auxTestCommand_Parse("RMX032"+goodCloSample, &CloRequest{}, &CloRequest{IdleMessage: goodCloSample}, true))
+
+}
+
+func TestCloRequest_String(t *testing.T) {
+	t.Run("00", auxTestCommand_String(&CloRequest{IdleMessage: goodCloSample}, "CLO032"+goodCloSample))
+	t.Run("bad size", auxTestCommand_String(&CloRequest{IdleMessage: "RETIRE O CARTAO"}, ""))
+}
+
+func TestCloResponse_GetStatus(t *testing.T) {
+	t.Run("status 0", auxTestCommand_GetStatus(&CloResponse{status: PP_OK}, PP_OK))
+}
+
+func TestCloResponse_Validate(t *testing.T) {
+	t.Run("empty", auxTestCommand_Validate(&CloResponse{status: 0}, false))
+}
+
+func TestCloResponse_Parse(t *testing.T) {
+	t.Run("status 0", auxTestCommand_Parse("CLO000000", &CloResponse{}, &CloResponse{status: PP_OK}, false))
+	t.Run("bad status", auxTestCommand_Parse("CLO00A0100000000000", &CloResponse{}, &CloResponse{}, true))
+	t.Run("Bad command name", auxTestCommand_Parse("RMX000000", &CloResponse{}, &CloResponse{}, true))
+	t.Run("bad command size", auxTestCommand_Parse("CLO00001A0000000000", &CloResponse{}, &CloResponse{}, true))
+	t.Run("bad param size", auxTestCommand_Parse("CLO0000110000000000", &CloResponse{}, &CloResponse{}, true))
+}
+
+func TestCloResponse_String(t *testing.T) {
+	t.Run("status 0", auxTestCommand_String(&CloResponse{status: PP_OK}, "CLO000000"))
+}
